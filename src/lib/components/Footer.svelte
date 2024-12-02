cef04c8bf4247c8032c63249e7044fb6c2ecbe1e
Anil Kumar <akt01.dev@gmail.com>
2024-12-02 18:19:01 +0530

    chore: add actual footer data

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,114 +1,67 @@
-<footer class="body-font text-gray-600 bg-purple-50">
+<script lang="ts">
+  import { Problems, Routes } from '$lib/constants';
+  import { ArrowRight, CaretRight } from 'phosphor-svelte';
+
+  const routes = Object.entries(Routes);
+
+  const problems = Object.entries(Problems);
+</script>
+
+<footer class="body-font bg-purple-50 text-gray-600">
   <div
     class="container mx-auto flex flex-col flex-wrap px-5 py-24 md:flex-row md:flex-nowrap md:items-center lg:items-start"
   >
     <div class="mx-auto w-64 flex-shrink-0 text-center md:mx-0 md:text-left">
       <a
+        href="/"
         class="title-font flex items-center justify-center font-medium text-gray-900 md:justify-start"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          class="h-10 w-10 rounded-full bg-indigo-500 p-2 text-white"
-          viewBox="0 0 24 24"
-        >
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span class="ml-3 text-xl">Lovluk</span>
+        <img width="220" src="/images/logo.svg" alt="Logo" />
       </a>
-      <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+      <p class="mt-6 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
     </div>
-    <div class="-mb-10 mt-10 flex flex-grow flex-wrap text-center md:mt-0 md:pl-20 md:text-left">
-      <div class="w-full px-4 md:w-1/2 lg:w-1/4">
-        <h2 class="title-font mb-3 text-sm font-medium tracking-widest text-gray-900">
-          CATEGORIES
+    <div class="-mb-10 mt-10 flex flex-col md:flex-row text-center md:mt-0 md:pl-20 md:text-left">
+      {@render links('Quick Links', routes)}
+      {@render links('Problems During Periods', problems)}
+
+      <div class="w-full px-4 md:w-1/2">
+        <h2 class="title-font mb-3 text-xl font-semibold  text-purple-700">
+          Contact Details
         </h2>
-        <nav class="mb-10 list-none">
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+        <ul class="space-y-4" >
+          <li class="flex items-start space-x-2" >
+            <CaretRight weight="bold" class="shrink-0" />
+            <span class="font-semibold" >Address</span>
+            <span class="text-gray-600">LovLuk India Enterprises
+              Park wali gali, near baba khetarnath mandir, sihma, Narnaul, Haryana 123028</span>
           </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          <li class="flex items-start space-x-2" >
+            <CaretRight weight="bold" class="shrink-0" />
+            <span class="font-semibold" >Email</span>
+            <a href="mailto:lovluk@gmail" class="text-blue-600">lovluk@gmail.com</a>
           </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          <li class="flex items-start space-x-2" >
+            <CaretRight weight="bold" class="shrink-0" />
+            <span class="font-semibold" >Phone</span>
+            <span class="text-gray-600">+91 9876543210</span>
           </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
-      <div class="w-full px-4 md:w-1/2 lg:w-1/4">
-        <h2 class="title-font mb-3 text-sm font-medium tracking-widest text-gray-900">
-          CATEGORIES
-        </h2>
-        <nav class="mb-10 list-none">
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
-      <div class="w-full px-4 md:w-1/2 lg:w-1/4">
-        <h2 class="title-font mb-3 text-sm font-medium tracking-widest text-gray-900">
-          CATEGORIES
-        </h2>
-        <nav class="mb-10 list-none">
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
-      <div class="w-full px-4 md:w-1/2 lg:w-1/4">
-        <h2 class="title-font mb-3 text-sm font-medium tracking-widest text-gray-900">
-          CATEGORIES
-        </h2>
-        <nav class="mb-10 list-none">
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
+        </ul>
       </div>
     </div>
   </div>
-  <img class="pb-4 mx-auto" src="https://ammycare.com/assets/img/made-in-india.png" alt="">
+  <img class="mx-auto pb-4" src="https://ammycare.com/assets/img/made-in-india.png" alt="" />
   <div class="border-t border-gray-200">
     <div class="container mx-auto flex flex-col flex-wrap px-5 py-4 sm:flex-row">
       <p class="text-center text-sm font-semibold text-gray-500 sm:text-left">
         © 2022 Lovluk | All rights reserved
       </p>
       <span class="mt-2 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start">
-        <a class="text-gray-500">
+        <a
+          href="https://www.facebook.com/LovlukIndiaEnterprises"
+          class="text-gray-500"
+          aria-label="Facebook"
+          target="_blank"
+        >
           <svg
             fill="currentColor"
             stroke-linecap="round"
@@ -120,21 +73,12 @@
             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
           </svg>
         </a>
-        <a class="ml-4 text-gray-500">
-          <svg
-            fill="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="h-5 w-5"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
-            ></path>
-          </svg>
-        </a>
-        <a class="ml-4 text-gray-500">
+        <a
+          href="https://www.instagram.com/lovluk_india_enterprises/"
+          class="ml-4 text-gray-500"
+          aria-label="Instagram"
+          target="_blank"
+        >
           <svg
             fill="none"
             stroke="currentColor"
@@ -148,24 +92,25 @@
             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
           </svg>
         </a>
-        <a class="ml-4 text-gray-500">
-          <svg
-            fill="currentColor"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="0"
-            class="h-5 w-5"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="none"
-              d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-            ></path>
-            <circle cx="4" cy="4" r="2" stroke="none"></circle>
-          </svg>
-        </a>
       </span>
     </div>
   </div>
 </footer>
+
+{#snippet links(title: string, iterator: [string, string][])}
+  <div class="w-full px-4 md:w-1/2">
+    <h2 class="title-font mb-3 text-xl font-semibold  text-purple-700">
+      {title}
+    </h2>
+    <nav class="mb-10 list-none space-y-2">
+      {#each iterator as [name, path] (path)}
+        {#if typeof path === 'string'}
+          <li class="flex items-center space-x-2" >
+            <ArrowRight size=16 />
+            <a href={path} class="text-gray-600 hover:text-gray-800">{name}</a>
+          </li>
+        {/if}
+      {/each}
+    </nav>
+  </div>
+{/snippet}
